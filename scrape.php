b3c221db490589b7f664d588875227acb66b4d63
Evan Grill <jetsonian@gmail.com>
2015-03-09 03:21:24 -0700

    03/08/2015 Nightly 2 [working]
    Scraper fills array with pack price data.  Database storage NYI.

--- scrape.php
+++ scrape.php
@@ -1,23 +1,52 @@
 <?php
-	$price_list = "prices_6.txt";
+	
+	$price_list = "./prices_6.txt";
 
-	if ( file_exists ( $price_list )  &&  is_readable ( $price_list ) )
+	$scraped_prices = array();
+
+	if ( file_exists ( $price_list ) )
 	{
+//		echo "file exists<br/>";
 		$booster_price_file = file_get_contents ( $price_list );
-		echo $booster_price_file;
-	/*	$price_array = preg_split ( "/[\s]+/", $booster_price_file );
+//		echo "file read<br/>";
+//		echo $booster_price_file;
+		$price_array = preg_split ( "/[\s]+/", $booster_price_file );
 
-		for ( $i = 0; $i < count ( $price_array ); $i++ )
+		$i = 0;
+		
+		while ( $price_array[$i][0] != '=' )
 		{
-			echo $price_array[$i];
+			$i++;
 		}
-*/
-	}
-	else
-	{
-		echo: "Error: Price List not found.";
-	}
+		$i += 3;
+//		echo $price_array[$i] . "<br/>";
+		while ( $i < count ( $price_array ) )
+		{
+			if ( $price_array[$i][0] == '[')
+			{
+//				echo substr( $price_array[$i], 1, 3 ) . "<br/>";
+				$temp_code = substr( $price_array[$i], 1, strlen ( $price_array[$i] ) - 2 );
+				$temp_buy = (double) $price_array[$i+1];
+				$temp_sell = (double) $price_array[$i+2];
 
+				$new_size = array_push ( $scraped_prices, array ( $temp_code, $temp_buy, $temp_sell ) );
 
+//				$temp_set = array ( $temp_code, $temp_buy, $temp_sell );
+//				echo $temp_set[0] . " ";
+//				echo $temp_set[1] . " ";
+//				echo $temp_set[2] . "<br/>";
+
+				$i += 2;
+			}
+			$i++;
+		}
+		for ($j = 0; $j < count ( $scraped_prices ); $j+=1 )
+		{
+			echo $j . ": ";
+			echo $scraped_prices[$j][0] . " ";
+			echo $scraped_prices[$j][1] . " ";
+			echo $scraped_prices[$j][2] . "<br/>";
+		}
+	}
 
 ?>
